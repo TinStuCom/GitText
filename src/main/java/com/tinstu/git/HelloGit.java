73e454e63530ba73061a371b30ce2c76520c3e04
TinStuCom <105421417+TinStuCom@users.noreply.github.com>
2022-05-13 19:50:43 +0800

    Update HelloGit.java

--- src/main/java/com/tinstu/git/HelloGit.java
+++ src/main/java/com/tinstu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("第一次提交");
         System.out.println("hot 第一次");
         System.out.println("提交hot之后，合并之前，修改master进行提交");
+        System.out.println("这里是在github在线修改的");
     }
 }
